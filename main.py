ea4872a3a70e2e20400cb969a7d218a37a4512c1
v4nixd <mishanikolchuk03@gmail.com>
2024-03-20 22:10:01 +0100

    Update main.py
    Added logging using icecream library

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
 
+import icecream
+from icecream import ic
+
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -22,21 +25,24 @@ async def on_message(message):
         return
 
     if isinstance(message.channel, discord.DMChannel):
-        if message.content.startswith('$rust'):
+        if message.content.startswith('$rust'): #$rust
             Embed = discord.Embed(title="Сервер мишани Rust", description="Памятка по серверу в расте", color=0xF58A42)
             Embed.add_field(name='Информация о сервере', value=f'IP - 104.234.252.167:28055 \n Последний вайп - {rustWipeDate} \n ', inline=False)
             await message.author.send(embed=Embed)
+            await ic(f'{message.author} used $rust using {message} in {message.channel}')
 
-        elif message.content.startswith('$clear'):
+        elif message.content.startswith('$clear'): #$clear
             async for message in message.channel.history(limit=100):
                 if message.author == client.user:
                     await message.delete()
                 else:
                     await message.channel.purge()
+                    await ic(f'{message.author} used $clear using {message} in {message.channel}')
     
     if message.channel.id == 1219984758252240969:
-        if message.content.startswith('$connect'):
+        if message.content.startswith('$connect'): #$connect
             connectEmbed = discord.Embed(title="Успешно подключено, дальнейшее пользование ботом будет происходить тут.", description="Напишите */help* чтобы узнать больше команд.", color=0x88fc03)
             await message.author.send(embed=connectEmbed)
+            await ic(f'{message.author} used $connect using {message} in {message.channel}')
 
 client.run('token')
